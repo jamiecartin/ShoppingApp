d169d83e975247c18b75551c49a9417b0d902da3
jamiecartin <jamie.cartin@icloud.com>
2023-03-20 14:52:37 -0400

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Jamie Cartin on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
